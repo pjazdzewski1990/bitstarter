312c84001383843c917974889d09da5a02d18d04
jfk <jfk@jfk-VirtualBox.(none)>
2013-07-06 11:38:59 +0200

    added index.html

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', function (err, html) {
+		if (err) {
+			console.log("Index.html: " +err);
+			throw err; 
+		}
+    response.write(html);  
+    response.end();  
+	});
 });
 
 var port = process.env.PORT || 5000;
